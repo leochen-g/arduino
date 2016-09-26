b52c725f2fc730e2b7d52ada780db1c63c2fd3ad
kiven <478914121@qq.com>
2016-09-26 23:30:06 +0800

    Arduino程序提交

---
+++ LM35/LM35.ino
@@ -0,0 +1,138 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <math.h>
+const int THERM_PIN = A3; //定义模拟端口A3 连接LM35温度传感器
+//  yeelink api
+#define APIKEY         "6d58041777974e0a50d508b78ac28db4" // APIKEY
+#define DEVICEID       345302 // 设备ID
+#define SENSORID       386351 // 传感器ID
+
+//初始化MAC地址
+byte mac[] = { 0x00, 0x1D, 0x72, 0x82, 0x35, 0x9D};
+// 初始化库函数
+EthernetClient client;
+char server[] = "api.yeelink.net";   // yeelink API
+
+
+unsigned long lastConnectionTime = 0;          // 上次连接服务器的时间 ms单位
+boolean lastConnected = false;                 // 最后一次的主循环
+const unsigned long postingInterval = 14 * 1000; // 延时30s发送数据
+
+void setup() {
+  Serial.begin(9600);//设置波特率为9600
+  //开始连接服务器
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    for (;;)
+      ;
+  }
+  else {
+    Serial.println("Ethernet configuration OK");
+  }
+}
+
+void loop() {
+  // 数据测试
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // 如果没有网络连接 再循环一次然后停止客户端
+  if (!client.connected() && lastConnected) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+
+  // 网络未连接，10s后重试
+  if (!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+    //读取数据
+    int sensorReading = readtemperSensor();
+    Serial.print("yeelink:");//输出数据
+    Serial.println(sensorReading);
+    //发送数据
+    sendData(sensorReading);
+  }
+  // 通过循环为下次链接做准备
+  lastConnected = client.connected();
+}
+
+// HTTP连接服务器发送数据 封装报文  学习东西最多的地方 首先要了解如何上传数据，数据格式是什么样的
+void sendData(int thisData) {
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+    //发送 HTTP PUT 要求
+    client.print("POST /v1.0/device/");
+    client.print(DEVICEID);
+    client.print("/sensor/");
+    client.print(SENSORID);
+    client.print("/datapoints");
+    client.println(" HTTP/1.1");
+    client.println("Host: api.yeelink.net");
+    client.print("Accept: *");
+    client.print("/");
+    client.println("*");
+    client.print("U-ApiKey: ");
+    client.println(APIKEY);
+    client.print("Content-Length: ");
+    // 计算传感器发送数据的字节
+    // 8个字节的数据加上数字的字节数
+    int thisLength = 10 + getLength(thisData);
+    client.println(thisLength);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.println();
+    // PUT请求的实际内容，从 API文档获得:
+    client.print("{\"value\":");
+    client.print(thisData);
+    client.println("}");
+  }
+  else {
+    // 链接失败提醒
+    Serial.println("connection failed");
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+  // 最后一次连接一下
+  lastConnectionTime = millis();
+}
+
+
+// 计算数字传感器读数  每一位的ASCII十进制表示一个字节,数字的数量等于字节数:
+int getLength(int someValue) {
+  int digits = 1;
+  // 值不断除以10,添加一个数字计数,直到值为0:
+  int dividend = someValue / 10;
+  while (dividend > 0) {
+    dividend = dividend / 10;
+    digits++;
+  }
+  // 返回数值
+  return digits;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// 传感器数值获取
+
+
+int readtemperSensor()
+{
+  int v ;
+  int dat;
+  v = analogRead(THERM_PIN);//读取传感器模拟值赋给v
+  dat = (125 * v) >> 8; //温度计算公式
+  /*学习知识
+    A/D出来是电压值，如果以mV表示的话，就是：V=Vref*val/1024，这里val是转换后的数值，是整形。
+    以为LM35输出电压与温度关系是：V=10mV*T，所以有：
+                 10mV*T=Vref*val/1024，这里，Vref=5000mV，即参考电压，
+           则：T=500/1024*val，将1024分为：256*4，
+           有：T=500/（4*256）*val=125/256*val，C语言位操作中，除以256，就是>>8，
+        所以：T=（125*val）>>8
+  */
+  Serial.print("Sensor value is: ");
+  Serial.println(dat);
+  delay(500);
+  return dat;
+}
---
+++ led/led.ino
@@ -0,0 +1,281 @@
+#include <SPI.h>  //调用通读接口库
+#include <Ethernet.h>  //调用网络模块库
+#include <Wire.h>  //调用I2C通讯库
+#include <math.h>  //调用函数库
+byte buff[2];  //定义缓冲区
+float cm;    //浮点型变量
+char flag = 0; //定义开关循环控制变量
+
+// for yeelink api
+
+#define APIKEY         "6d58041777974e0a50d508b78ac28db4" // 替换你的 API KEY
+#define DEVICEID       345302 // 替换你的device ID
+String SENSORno[] = {"386996","386997","386998"}; // 把sensor ID 依次替在这里，程序定义为数组，几个开关就写几个
+
+//for led pin
+
+byte ledPin[] = {A0,A1,A2}; //定义输出的针脚A0-LED1 A2-LED2  A3-LED3
+
+// assign a MAC address for the ethernet controller.
+
+byte mac[] = { 0x00, 0x1D, 0x72, 0x82, 0x35, 0x9D};
+
+// initialize the library instance:
+
+EthernetClient client;
+
+char server[] = "api.yeelink.net";   // name address for yeelink API
+
+unsigned long lastConnectionTime = 0;          // 初始化链接时间
+
+boolean lastConnected = false;                 // state of the connection last time through the main loop
+
+const unsigned long postingInterval = 3 * 1000; // delay between 2 datapoints, 3s
+
+String returnValue = "";
+
+boolean ResponseBegin = false;
+
+
+
+
+void setup() {
+
+  for (int i = 0; i < 3; i++) //3个开关，循环次数
+
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+
+
+  Wire.begin();
+
+  // start serial port:
+
+  Serial.begin(9600);
+
+  // start the Ethernet connection with DHCP:
+
+  if (Ethernet.begin(mac) == 0) {
+
+    Serial.println("Failed to configure Ethernet using DHCP");
+
+    for (;;)
+
+      ;
+
+  }
+
+  else {
+
+    Serial.println("Ethernet configuration OK");
+
+  }
+
+}
+
+
+
+
+void loop() {
+
+  // if there's incoming data from the net connection.
+
+  // send it out the serial port.  This is for debugging
+
+  // purposes only:
+
+  if (client.available()) {
+
+    char c = client.read();
+
+    // Serial.print(c);
+
+    if (c == '{')
+
+      ResponseBegin = true;
+
+    else if (c == '}')
+
+      ResponseBegin = false;
+    if (ResponseBegin)
+
+      returnValue += c;
+
+  }
+
+
+  if (returnValue.length() != 0 && (ResponseBegin == false))
+  {
+
+    Serial.println(returnValue);
+
+    if (returnValue.charAt(returnValue.length() - 1) == '1') {
+
+      Serial.println("turn on the LED");
+
+      //  digitalWrite( 7, HIGH);
+
+      digitalWrite( ledPin[flag], HIGH);
+
+    }
+
+    else if (returnValue.charAt(returnValue.length() - 1) == '0') {
+
+      Serial.println("turn off the LED");
+
+      //  digitalWrite(7, LOW);
+
+      digitalWrite( ledPin[flag], LOW);
+    }
+
+    returnValue = "";
+
+    flag ++;
+
+  }
+
+
+
+  // if there's no net connection, but there was one last time
+
+  // through the loop, then stop the client:
+
+  if (!client.connected() && lastConnected) {
+
+    Serial.println();
+
+    Serial.println("disconnecting.");
+
+    client.stop();
+
+  }
+
+  // if you're not connected, and ten seconds have passed since
+
+  // your last connection, then connect again and send data:
+
+  if (!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+
+    // read sensor data, replace with your code
+
+    //int sensorReading = readLightSensor();
+
+    Serial.print("yeelink:");
+    if (flag < 3) // 7改成开关数量
+    {
+
+      //get data from server
+
+      getData();
+
+    }
+
+    else
+
+    {
+
+      //put data to server
+
+      //     postData(readData());
+
+      if (flag = 3) {
+        flag = 0; // 7改成开关数量
+      }
+
+    }
+
+
+
+
+    // note the time that the connection was made or attempted:
+
+    lastConnectionTime = millis();
+
+  }
+
+  // store the state of the connection for next time through
+
+  // the loop:
+
+  lastConnected = client.connected();
+
+
+
+}
+
+
+
+
+
+
+
+// this method makes a HTTP connection to the server and get data back
+
+void getData(void) {
+
+  // if there's a successful connection:
+
+  if (client.connect(server, 80)) {
+
+    Serial.println("connecting...");
+
+    // send the HTTP GET request:
+
+    client.print("GET /v1.0/device/");
+
+    client.print(DEVICEID);
+
+    client.print("/sensor/");
+
+    //   client.print(SENSORID);
+
+    client.print(SENSORno[flag]);
+
+    client.print("/datapoints");
+
+    client.println(" HTTP/1.1");
+
+    client.println("Host: api.yeelink.net");
+
+    client.print("Accept: *");
+
+    client.print("/");
+
+    client.println("*");
+
+    client.print("U-ApiKey: ");
+
+    client.println(APIKEY);
+
+    client.println("Content-Length: 0");
+
+    client.println("Connection: close");
+
+    client.println();
+
+    Serial.println("print get done.");
+
+  }
+
+  else
+
+  {
+
+    // if you couldn't make a connection:
+
+    Serial.println("connection failed");
+
+    Serial.println();
+
+    Serial.println("disconnecting.");
+
+    client.stop();
+
+  }
+
+  // note the time that the connection was made or attempted:
+
+  //  lastConnectionTime = millis();
+  lastConnectionTime = millis();
+}
